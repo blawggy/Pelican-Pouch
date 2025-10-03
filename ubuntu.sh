c7e2dade518f8cfd28a40edf046ee6e49dec0c79
ZPTC <138015613+blawggy@users.noreply.github.com>
2025-10-03 10:31:49 +0200

    it must work now :3

--- ubuntu.sh
+++ ubuntu.sh
@@ -34,22 +34,20 @@ install_docker() {
     
     info "Installing Docker via official repository"
     (
-        apt update >/dev/null 2>&1
-        apt install -y ca-certificates curl gnupg lsb-release >/dev/null 2>&1
         # Add Docker's official GPG key:
-        sudo apt-get update >/dev/null 2>&1
-        sudo install -m 0755 -d /etc/apt/keyrings >/dev/null 2>&1
-        sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc >/dev/null 2>&1
-        sudo chmod a+r /etc/apt/keyrings/docker.asc >/dev/null 2>&1
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-        # Add the repository to Apt sources:
-        echo \
-          "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-          $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-          sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-        sudo apt-get update >/dev/null 2>&1
-        apt update >/dev/null 2>&1
-        apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin >/dev/null 2>&1
+    # Add the repository to Apt sources:
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+      $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
     ) & show_spinner $!
     
     systemctl enable --now docker
